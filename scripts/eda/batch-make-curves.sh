7b581ff2627a75a514d691e6d0450272dd78bec6
Joseph Turian <turian@gmail.com>
2009-11-20 18:10:50 -0500

    Small fix in batch script

--- scripts/eda/batch-make-curves.sh
+++ scripts/eda/batch-make-curves.sh
@@ -30,8 +30,8 @@ echo > graphs-validationlogrankloss.gp
 echo "set terminal postscript color 12" >> graphs-validationlogrankloss.gp
 echo "set output 'graphs-validationlogrankloss.ps'" >> graphs-validationlogrankloss.gp
 #echo "set logscale y" >> graphs-validationlogrankloss.gp
-echo "plot [] [:2] \\" >> graphs-validationlogrankloss.gp
-~/dev/common-scripts/sort-curves.py */*validationlogrankloss.dat | perl -ne "chop; print \"\\t'\$_' with lp, \\\\\\n\""  | perl -e '$str = ""; while(<>){ $str .= $_; } $str =~ s/, \\$//s; print $str' >> graphs-validationlogrankloss.gp
+echo "plot [] [] \\" >> graphs-validationlogrankloss.gp
+~/dev/common-scripts/sort-curves.py *validationlogrankloss.dat | perl -ne "chop; print \"\\t'\$_' with lp, \\\\\\n\""  | perl -e '$str = ""; while(<>){ $str .= $_; } $str =~ s/, \\$//s; print $str' >> graphs-validationlogrankloss.gp
 
 gnuplot graphs-trainerror.gp
 gnuplot graphs-trainloss.gp
