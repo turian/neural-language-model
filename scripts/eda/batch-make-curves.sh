53d0370c573604a56d3196e892095fd42c0ee9ce
Joseph Turian <turian@gmail.com>
2009-12-12 17:35:28 -0500

    Plot curves using lines, not linepoints.

--- scripts/eda/batch-make-curves.sh
+++ scripts/eda/batch-make-curves.sh
@@ -20,21 +20,21 @@ echo "set terminal postscript color 12" >> graphs-trainerror.gp
 echo "set output 'graphs-trainerror.ps'" >> graphs-trainerror.gp
 echo "set logscale y" >> graphs-trainerror.gp
 echo "plot [] [] \\" >> graphs-trainerror.gp
-~/dev/common-scripts/sort-curves.py *trainerror.dat | perl -ne "chop; print \"\\t'\$_' with lp, \\\\\\n\"" | perl -e '$str = ""; while(<>){ $str .= $_; } $str =~ s/, \\$//s; print $str' >> graphs-trainerror.gp
+~/dev/common-scripts/sort-curves.py *trainerror.dat | perl -ne "chop; print \"\\t'\$_' with l lw 2, \\\\\\n\"" | perl -e '$str = ""; while(<>){ $str .= $_; } $str =~ s/, \\$//s; print $str' >> graphs-trainerror.gp
 
 echo > graphs-trainloss.gp
 echo "set terminal postscript color 12" >> graphs-trainloss.gp
 echo "set output 'graphs-trainloss.ps'" >> graphs-trainloss.gp
 echo "set logscale y" >> graphs-trainloss.gp
 echo "plot [] [] \\" >> graphs-trainloss.gp
-~/dev/common-scripts/sort-curves.py *trainloss.dat | perl -ne "chop; print \"\\t'\$_' with lp, \\\\\\n\""  | perl -e '$str = ""; while(<>){ $str .= $_; } $str =~ s/, \\$//s; print $str' >> graphs-trainloss.gp
+~/dev/common-scripts/sort-curves.py *trainloss.dat | perl -ne "chop; print \"\\t'\$_' with l lw 2, \\\\\\n\""  | perl -e '$str = ""; while(<>){ $str .= $_; } $str =~ s/, \\$//s; print $str' >> graphs-trainloss.gp
 
 echo > graphs-validationlogrankloss.gp
 echo "set terminal postscript color 12" >> graphs-validationlogrankloss.gp
 echo "set output 'graphs-validationlogrankloss.ps'" >> graphs-validationlogrankloss.gp
 #echo "set logscale y" >> graphs-validationlogrankloss.gp
 echo "plot [] [] \\" >> graphs-validationlogrankloss.gp
-~/dev/common-scripts/sort-curves.py *validationlogrankloss.dat | perl -ne "chop; print \"\\t'\$_' with lp, \\\\\\n\""  | perl -e '$str = ""; while(<>){ $str .= $_; } $str =~ s/, \\$//s; print $str' >> graphs-validationlogrankloss.gp
+~/dev/common-scripts/sort-curves.py *validationlogrankloss.dat | perl -ne "chop; print \"\\t'\$_' with l lw 2, \\\\\\n\""  | perl -e '$str = ""; while(<>){ $str .= $_; } $str =~ s/, \\$//s; print $str' >> graphs-validationlogrankloss.gp
 
 gnuplot graphs-trainerror.gp
 gnuplot graphs-trainloss.gp
