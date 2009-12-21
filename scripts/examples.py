6924052d49cf89b3f176f96989ae798dbc8f21ea
Joseph Turian <turian@iro.umontreal.ca>
2009-12-21 16:01:33 -0500

    Converted get_train_example generator into TrainingExampleStream class with __iter__ method

--- scripts/examples.py
+++ scripts/examples.py
@@ -7,26 +7,30 @@ import string
 
 import common.hyperparameters
 
-def get_train_example():
-    HYPERPARAMETERS = common.hyperparameters.read("language-model")
-
-    from vocabulary import wordmap
-    for l in myopen(HYPERPARAMETERS["TRAIN_SENTENCES"]):
-        prevwords = []
-        for w in string.split(l):
-            w = string.strip(w)
-            id = None
-            if wordmap.exists(w):
-                prevwords.append(wordmap.id(w))
-                if len(prevwords) >= HYPERPARAMETERS["WINDOW_SIZE"]:
-                    yield prevwords[-HYPERPARAMETERS["WINDOW_SIZE"]:]
-            else:
-                prevwords = []
+class TrainingExampleStream(object):
+    def __init__(self):
+        pass
+    
+    def __iter__(self):
+        HYPERPARAMETERS = common.hyperparameters.read("language-model")
+        from vocabulary import wordmap
+        for l in myopen(HYPERPARAMETERS["TRAIN_SENTENCES"]):
+            prevwords = []
+            for w in string.split(l):
+                w = string.strip(w)
+                id = None
+                if wordmap.exists(w):
+                    prevwords.append(wordmap.id(w))
+                    if len(prevwords) >= HYPERPARAMETERS["WINDOW_SIZE"]:
+                        yield prevwords[-HYPERPARAMETERS["WINDOW_SIZE"]:]
+                else:
+                    prevwords = []
 
 def get_train_minibatch():
     HYPERPARAMETERS = common.hyperparameters.read("language-model")
     minibatch = []
-    for e in get_train_example():
+    get_train_example = TrainingExampleStream()
+    for e in get_train_example:
         minibatch.append(e)
         if len(minibatch) >= HYPERPARAMETERS["MINIBATCH SIZE"]:
             assert len(minibatch) == HYPERPARAMETERS["MINIBATCH SIZE"]
