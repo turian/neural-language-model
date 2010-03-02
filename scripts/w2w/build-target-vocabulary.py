3f8f31aa170ef077042c03abc6e4f8cfa7309255
Joseph Turian <turian@gmail.com>
2010-03-01 19:22:02 -0500

    Refactored code into method w2w.corpora.bicorpus_sentences_and_alignments

--- scripts/w2w/build-target-vocabulary.py
+++ scripts/w2w/build-target-vocabulary.py
@@ -7,70 +7,27 @@ words are equally weighted.)
 """
 
 import sys
-import itertools
 
 if __name__ == "__main__":
-    from common.stats import stats
     import common.hyperparameters, common.options
     HYPERPARAMETERS = common.hyperparameters.read("language-model")
     HYPERPARAMETERS, options, args, newkeystr = common.options.reparse(HYPERPARAMETERS)
     import hyperparameters
 
-    from common.str import percent
-
     import w2w.corpora
     import w2w.vocabulary
-    import string
     from collections import defaultdict
     from common.mydict import sort as dictsort
 
-    print >> sys.stderr, stats()
-
     cnt = {}
     for l1, l2, f1, f2, falign in w2w.corpora.bicorpora_filenames():
-        i = 0
-        emptycnt = 0
-        print >> sys.stderr, "\n"
-        print >> sys.stderr, "Processing %s, %s, %s, %s, %s" % (l1, l2, f1, f2, falign)
-        fil1, fil2, filalign = open(f1), open(f2), open(falign)
-        for (s1, s2, salign) in itertools.izip(fil1, fil2, filalign):
-#            print s1, s2, salign,
-            i += 1
-            if i % 100000 == 0:
-                print >> sys.stderr, "\tRead line %d of %s, %s, %s..." % (i, f1, f2, falign)
-                print >> sys.stderr, "\tEmpty sentences are %s..." % (percent(emptycnt, i))
-                print >> sys.stderr, "\t%s" % stats()
-            # Read the two sentences and convert them to IDs.
-            ws1 = [w2w.vocabulary.wordmap.id((l1, w1)) for w1 in string.split(s1)]
-            ws2 = [w2w.vocabulary.wordmap.id((l2, w2)) for w2 in string.split(s2)]
-
-            if len(ws1) == 0 or len(ws2) == 0:
-                emptycnt += 1
-                continue
-
-#            print ws2, [w2w.vocabulary.wordmap.str(w2) for w2 in ws2]
-            for link in string.split(salign):
-                i1, i2 = string.split(link, sep="-")
-                i1, i2 = int(i1), int(i2)
+        for ws1, ws2, links in w2w.corpora.bicorpus_sentences_and_alignments(l1, l2, f1, f2, falign):
+            for i1, i2 in links:
                 w1 = ws1[i1]
                 w2 = ws2[i2]
                 if w1 not in cnt: cnt[w1] = defaultdict(int)
 #                print w2w.vocabulary.wordmap.str(w1)[1], w2w.vocabulary.wordmap.str(w2)[1]
                 cnt[w1][w2] += 1
-
-        # Make sure all iterators are exhausted
-        alldone = 0
-        try: value = fil1.next()
-        except StopIteration: alldone += 1
-        try: value = fil2.next()
-        except StopIteration: alldone += 1
-        try: value = filalign.next()
-        except StopIteration: alldone += 1
-        assert alldone == 3
-
-        print >> sys.stderr, "DONE. Read line %d of %s, %s, %s..." % (i, f1, f2, falign)
-        print >> sys.stderr, "Empty sentences are %s..." % (percent(emptycnt, i))
-        print >> sys.stderr, stats()
 
     for w1 in cnt:
         print w2w.vocabulary.wordmap.str(w1), [(n, w2w.vocabulary.wordmap.str(w2)) for n, w2 in dictsort(cnt[w1])]
--- scripts/w2w/corpora.py
+++ scripts/w2w/corpora.py
@@ -3,8 +3,14 @@ Methods for reading corpora.
 """
 
 from os.path import join, isdir, exists
+import sys
 import os
 import re
+import itertools
+import string
+
+from common.stats import stats
+from common.str import percent
 
 def bicorpora_filenames():
     """
@@ -38,6 +44,58 @@ def monocorpora_filenames():
     # Not yet implemented
     assert len(HYPERPARAMETERS["W2W MONOCORPORA"]) == 0
     return []
+
+def bicorpus_sentences_and_alignments(l1, l2, f1, f2, falign):
+    """
+    Given languages l1 and l2 and their bicorpus filenames f1, f2, and falign,
+    yield tuples of the former (ws1, ws2, links),
+    where ws1 are the word ids in the sentence from f1,
+    where ws2 are the word ids in the sentence from f2,
+    and links is a list of (i1, i2) word indexes that are linked.
+    """
+    from w2w.vocabulary import wordmap
+
+    i = 0
+    emptycnt = 0
+    print >> sys.stderr, "\n"
+    print >> sys.stderr, "Reading %s,%s sentences and alignments from %s, %s, %s" % (l1, l2, f1, f2, falign)
+    fil1, fil2, filalign = open(f1), open(f2), open(falign)
+    for (s1, s2, salign) in itertools.izip(fil1, fil2, filalign):
+   #     print s1, s2, salign,
+        i += 1
+        if i % 100000 == 0:
+            print >> sys.stderr, "\tRead line %d of %s, %s, %s..." % (i, f1, f2, falign)
+            print >> sys.stderr, "\tEmpty sentences are %s..." % (percent(emptycnt, i))
+            print >> sys.stderr, "\t%s" % stats()
+
+        ws1 = [(l1, w1) for w1 in string.split(s1)]
+        ws2 = [(l2, w2) for w2 in string.split(s2)]
+        ws1 = [wordmap.id(tok) for tok in ws1]
+        ws2 = [wordmap.id(tok) for tok in ws2]
+   
+        if len(ws1) == 0 or len(ws2) == 0:
+            emptycnt += 1
+            continue
+   
+   #     print ws2, [w2w.vocabulary.wordmap.str(w2) for w2 in ws2]
+        links = [string.split(link, sep="-") for link in string.split(salign)]
+        links = [(int(i1), int(i2)) for i1, i2 in links]
+
+        yield ws1, ws2, links
+   
+    # Make sure all iterators are exhausted
+    alldone = 0
+    try: value = fil1.next()
+    except StopIteration: alldone += 1
+    try: value = fil2.next()
+    except StopIteration: alldone += 1
+    try: value = filalign.next()
+    except StopIteration: alldone += 1
+    assert alldone == 3
+   
+    print >> sys.stderr, "DONE. Read line %d of %s, %s, %s..." % (i, f1, f2, falign)
+    print >> sys.stderr, "Empty sentences are %s..." % (percent(emptycnt, i))
+    print >> sys.stderr, stats()
 
 if __name__ == "__main__":
     for l1, l2, f1, f2, falign in bicorpora_filenames():
