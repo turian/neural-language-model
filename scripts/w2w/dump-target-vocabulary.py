83a1bcf07053a8524d7ff461ee85366e6ed6e4a5
Joseph Turian <turian@gmail.com>
2010-03-01 23:41:35 -0500

    Fixed up scripts/w2w/dump-target-vocabulary.py

--- scripts/w2w/dump-target-vocabulary.py
+++ scripts/w2w/dump-target-vocabulary.py
@@ -12,10 +12,22 @@ if __name__ == "__main__":
     import hyperparameters
 
     from common.mydict import sort as dictsort
+    from common.str import percent
 
-    from vocabulary import wordmap
+    from vocabulary import wordmap, wordform, language
     from targetvocabulary import targetmap
 
     for w1 in wordmap().all:
+        w1 = wordmap().id(w1)
+        # Actually, should assert W2W SKIP TRANSLATIONS FROM UNKNOWN WORD
+        assert HYPERPARAMETERS["W2W SKIP TRANSLATIONS TO UNKNOWN WORD"]
+        if language(w1) is None:
+            print >> sys.stderr, "Skipping %s" % `wordmap().str(w1)`
+            continue
+        if w1 not in targetmap():
+            print >> sys.stderr, "Skipping %s, not a source word in targetmap" % `wordmap().str(w1)`
+            continue
         for l2 in targetmap()[w1]:
-            print w1, l2, dictsort(targetmap()[w1][l2])
+            totcnt = 0
+            for cnt, w2 in dictsort(targetmap()[w1][l2]): totcnt += cnt
+            print wordmap().str(w1), l2, [(percent(cnt, totcnt), wordform(w2)) for cnt, w2 in dictsort(targetmap()[w1][l2])]
